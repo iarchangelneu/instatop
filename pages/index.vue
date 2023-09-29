29a29108f6d72790b04bd8ce2ec0cf6bdf8f1dc8
iarchangelneu <iarchangel@bk.ru>
2023-09-29 17:26:04 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="page"></div>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    }
+}
+</script>
+<script setup>
+useSeoMeta({
+    title: 'Главная | InstaTop',
+    ogTitle: 'Главная | InstaTop',
+    description: 'Главная | InstaTop',
+    ogDescription: 'Главная | InstaTop',
+})
+</script>
+<style lang="scss" scoped></style>
